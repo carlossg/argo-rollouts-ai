e953925ce57c424f158d460474627710544ecab2
Carlos Sanchez <carlos@apache.org>
2024-10-01 15:57:25 +0100

    feat: compare stable and canary logs
    for canary analysis

--- README.md
+++ README.md
@@ -1,9 +1,79 @@
 # Argo Rollouts Analysis using AI
 
-Example Argo Rollouts Analysis that uses AI services to provide a better explanation
-of what has happened during rollout.
+Example Argo Rollouts Analysis that uses AI services to do the canary analysis.
 
-# Setup
+This job can be executed as a Argo Rollouts canary analysis job.
+It will fetch the logs from pods with the label `role=stable` and `role=canary`
+and use an AI model to compare both logs to decide whether the canary promotion
+should continue or not.
+
+The result is returned from the model as a json file like:
+
+```json
+{
+    "text": "Canary deployment encountered Bad Request errors (HTTP 400) due to image pulling failures, indicating a problem with the canary version's image or configuration.  The stable version did not exhibit these errors.",
+    "promote": false,
+    "confidence": 95
+}
+```
+
+Tested with:
+
+* Google Gemini
+
+# Usage
+
+Argo Rollouts allows different sources for analysis, one of them being the execution of a job.
+
+In your `Rollout` object set the `spec.metrics.provider.job` to something like the following.
+[Full example](examples/analysis-success-rate-job-ai.yaml).
+
+```yaml
+        metadata:
+          annotations:
+            service: "{{args.service}}"
+          labels:
+            service: "{{args.service}}"
+        spec:
+          backoffLimit: 0
+          ttlSecondsAfterFinished: 1200
+          template:
+            metadata:
+              labels:
+                app: success-rate-job-ai
+            spec:
+              restartPolicy: Never
+              containers:
+              - name: ai
+                image: us-central1-docker.pkg.dev/api-project-642841493686/github/argo-rollouts-ai:580503203f0460f299e1f4de569b5876c597a37b
+                imagePullPolicy: IfNotPresent
+                env:
+                - name: GOOGLE_API_KEY
+                  valueFrom:
+                    secretKeyRef:
+                      name: success-rate-job-ai
+                      key: google_api_key
+                volumeMounts:
+                - name: empty
+                  mountPath: /logs
+                # - name: code
+                #   mountPath: /app
+                resources:
+                  requests:
+                    memory: "32Mi"
+                    cpu: "250m"
+                  limits:
+                    memory: "64Mi"
+                    cpu: "500m"
+              volumes:
+              - name: empty
+                emptyDir: {}
+              # - name: code
+              #   configMap:
+              #     name: canary-demo-6mm6k4ffck
+```
+
+# Development Setup
 
 ```
 python3 -m venv venv
@@ -44,3 +114,9 @@ docker run -ti --rm \
 ```shell
 make build
 ```
+
+# Notes
+
+Google Cloud credits are provided for this project.
+
+#AISprint
--- app.py
+++ app.py
@@ -10,44 +10,120 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.callbacks import get_openai_callback
 from dotenv import load_dotenv
 
+from kubernetes import client, config
+
 # read .env variables
 load_dotenv()
 
 # if /logs exists use it, otherwise use app.log
-log_file_name = "/logs/app.log" if os.path.exists("/logs") else "app.log"
+log_dir = "/logs" if os.path.exists("/logs") else "./logs"
+log_stable_file_name = os.path.join(log_dir, "app-stable.log")
+log_canary_file_name = os.path.join(log_dir, "app-canary.log")
 
 # first argument is service url
 url = sys.argv[1] if len(sys.argv) > 1 else None
-if not url:
-    print("Please provide the url as an argument.")
-    sys.exit(1)
+# if not url:
+#     print("Please provide the url as an argument.")
+#     sys.exit(1)
 
 # initial sleep
-sleep = 1
+sleep = 0
 print(f"Sleeping for {sleep} seconds")
 time.sleep(sleep)
 
 return_code = 0
 
-# Open the log file
-with open(log_file_name, "a") as log_file:
-    for i in range(10):  # Loop 10 times
+# Configure the Kubernetes client
+config.load_kube_config()
+
+# Create an API client
+v1 = client.CoreV1Api()
+
+
+# Get the current namespace from the kubeconfig file or from inside the pod
+def get_current_namespace(context: str = None) -> str | None:
+    ns_path = "/var/run/secrets/kubernetes.io/serviceaccount/namespace"
+    if os.path.exists(ns_path):
+        with open(ns_path) as f:
+            return f.read().strip()
+    try:
+        contexts, active_context = config.list_kube_config_contexts()
+        if context is None:
+            return active_context["context"]["namespace"]
+        selected_context = next(ctx for ctx in contexts if ctx["name"] == context)
+        return selected_context["context"]["namespace"]
+    except (KeyError, StopIteration):
+        return "default"
+
+
+# Get the logs from pods with the label 'role'
+def get_logs(namespace: str, role: str, log_file_name: str) -> bool:
+    with open(log_file_name, "a") as log_file:
         try:
-            # Make the request with a timeout of 1 second
-            print(f"Making request {i + 1} to {url}")
-            response = requests.get(url, timeout=1)
-            if response.status_code != 200:
-                return_code = 1
-            # Write the response text to the log file
-            print(f"Response: {response.status_code} {response.text}")
-            log_file.write(f"{response.status_code} {response.text}\n")
+            # List all pods in this namespace with the label 'stable'
+            pods = v1.list_namespaced_pod(
+                namespace=namespace, label_selector=f"role={role}"
+            )
+            if not pods.items:
+                print(f"No pods found for role {role} in namespace {namespace}")
+                return 1
+            for pod in pods.items:
+                pod_name = pod.metadata.name
+                namespace = pod.metadata.namespace
+                print(
+                    f"Getting logs for {role} pod {pod_name} in namespace {namespace}"
+                )
+                response = v1.read_namespaced_pod_log(
+                    name=pod_name, namespace=namespace
+                )
+
+                # Write the response text to the log file
+                log_file.write(f"Pod: {pod_name}\n{response}\n")
+                print(f"Got logs for pod {pod_name}")
+                return 0
         except requests.RequestException as e:
-            return_code = 1
             print(f"Error: {e}")
             # If an error occurs, write the error to the log file
             log_file.write(str(e) + "\n")
-        # Wait for 1 second before making the next request
-        time.sleep(1)
+            return 1
+        except client.exceptions.ApiException as e:
+            print(f"API Error: {e}")
+            # If an API error occurs, write the error to the log file
+            log_file.write(str(e) + "\n")
+            return 1
+
+
+# get current namespace from kubeconfig
+namespace = get_current_namespace(context="default")  # TODO
+if not namespace:
+    print("Namespace not found")
+    sys.exit(1)
+print(f"Current namespace: {namespace}")
+
+if get_logs(namespace, "stable", log_stable_file_name) != 0:
+    sys.exit(1)
+if not get_logs(namespace, "canary", log_canary_file_name):
+    sys.exit(1)
+
+# # Get the logs from stable pods
+# with open(log_file_name_stable, "a") as log_file:
+#     try:
+#         # get pods logs
+
+#         # Write the response text to the log file
+#         print(f"Got stable logs")
+#         log_file.write(f"{response.status_code} {response.text}\n")
+#     except requests.RequestException as e:
+#         return_code = 1
+#         print(f"Error: {e}")
+#         # If an error occurs, write the error to the log file
+#         log_file.write(str(e) + "\n")
+#     # Wait for 1 second before making the next request
+#     time.sleep(1)
+
+# Get the logs from canary pods
+
+# TODO
 
 # create a model
 model = ChatGoogleGenerativeAI(
@@ -61,10 +137,13 @@ model = ChatGoogleGenerativeAI(
 )
 
 system_template = (
-    "Analyze what was this canary behavior based on this curl output."
-    + "After your analysis, in the last line write a json formatted line with two entries, "
-    + "one named 'promote' with 'true' or 'false' depending on whether canary promotion should continue,"
-    + "and another named 'confidence' with a number from 0 to 100 representing your confidence in the decision."
+    "Analyze what was this canary behavior based on these logs, compare the stable version vs the canary version."
+    + "After your analysis, in the last line write a json formatted line with these entries: "
+    + "one named 'text' with your analysis text; "
+    + "one named 'promote' with 'true' or 'false' depending on whether canary promotion should continue; "
+    + "one named 'confidence' with a number from 0 to 100 representing your confidence in the decision. "
+    + "The stable version logs start with '--- STABLE LOGS ---' "
+    + "and the canary version logs start with '--- CANARY LOGS ---' "
 )
 
 prompt_template = ChatPromptTemplate.from_messages(
@@ -72,17 +151,22 @@ prompt_template = ChatPromptTemplate.from_messages(
 )
 parser = StrOutputParser()
 
-# analyze each log file
-
+# analyze logs using the model
 total_cost = 0
-print(f"\n\nAnalyzing log file: {log_file_name}\n")
-with open(log_file_name, "r") as f:
-    lines = f.readlines()
-    with get_openai_callback() as cb:
-        chain = prompt_template | model | parser
-        result = chain.invoke({"text": lines})
-        total_cost += cb.total_cost
-        print(f"{result}\n")
+print(f"\n\nAnalyzing log files: {log_stable_file_name} and {log_canary_file_name}\n")
+with open(log_stable_file_name, "r") as stable_file:
+    with open(log_canary_file_name, "r") as canary_file:
+        lines_stable = stable_file.readlines()
+        lines_canary = canary_file.readlines()
+        with get_openai_callback() as cb:
+            chain = prompt_template | model | parser
+            result = chain.invoke(
+                {
+                    "text": f"--- STABLE LOGS ---\n{lines_stable}\n\n--- CANARY LOGS ---\n{lines_canary}"
+                }
+            )
+            total_cost += cb.total_cost
+            print(f"{result}\n")
 
 # print the cost
 print(
